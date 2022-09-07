9dcf066551d89da7cff7a37feb5da5247a5738f4
Sergio Munoz <sergio.munoz@pubnub.com>
2022-09-07 15:05:32 -0400

    Updated cli/v1.py. Includes tests.

--- pubnub_python_tools/cli/v1.py
+++ pubnub_python_tools/cli/v1.py
@@ -1,29 +1,38 @@
+"""CLI v1 Argument Parser."""
 import argparse
 
+DESCRIPTION = "Quickly interact with PubNub's Python SDK."
+
+# Create argument parser
+parser = argparse.ArgumentParser(add_help=True, description=DESCRIPTION)
+
+# Add arguments to parser
+parser.add_argument("-sk", "--subscribe-key", type=str, help="PubNub SubscribeKey")
+parser.add_argument("-pk", "--publish-key", type=str, help="PubNub PublishKey")
+parser.add_argument("-u", "--uuid", type=str, help="PubNub UUID")
+parser.add_argument("-s", "--subscribe", type=str, help="Subscribe to a Channel")
+parser.add_argument("-pres", "--presence", action='store_true', help="Subscribe with Presence")
+parser.add_argument("-p", "--publish", type=str, help="Publish a message to a Channel")
+parser.add_argument("-m", "--message", type=str, help="Message to publish")
+parser.add_argument("-us", "--unsubscribe", type=str, help="Unsubscribe from a Channel")
+parser.add_argument("-here", "--here-now", type=str, help="Here now on a Channel")
+parser.add_argument("-dm", "--dev-man", type=str, help="Attach a Device Manager to file")
+
 def get_parser(args):
-    parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("-sk", "--subscribe-key", type=str, help="PubNub SubscribeKey")
-    parser.add_argument("-pk", "--publish-key", type=str, help="PubNub PublishKey")
-    parser.add_argument("-u", "--uuid", type=str, help="PubNub UUID")
-    parser.add_argument("-s", "--subscribe", type=str, help="Subscribe to a Channel")
-    parser.add_argument("-pres", "--presence", action='store_true', help="Subscribe with Presence")
-    parser.add_argument("-p", "--publish", type=str, help="Publish a message to a Channel")
-    parser.add_argument("-m", "--message", type=str, help="Message to publish")
-    parser.add_argument("-us", "--unsubscribe", type=str, help="Unsubscribe from a Channel")
-    parser.add_argument("-here", "--here-now", type=str, help="Unsubscribe from a Channel")
-    parser.add_argument("-dm", "--device-manager", type=str, help="Attach a local Device Manager at a file")
+    """Get argument parser
+
+    Args:
+        args: user arguments
+
+    Returns:
+        parser: parser with arguments parsed
+    """
     return parser.parse_args(args)
-    
+
 def create_parser():
-    parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("-sk", "--subscribe-key", type=str, help="PubNub SubscribeKey")
-    parser.add_argument("-pk", "--publish-key", type=str, help="PubNub PublishKey")
-    parser.add_argument("-u", "--uuid", type=str, help="PubNub UUID")
-    parser.add_argument("-s", "--subscribe", type=str, help="Subscribe to a Channel")
-    parser.add_argument("-pres", "--presence", action='store_true', help="Subscribe with Presence")
-    parser.add_argument("-p", "--publish", type=str, help="Publish a message to a Channel")
-    parser.add_argument("-m", "--message", type=str, help="Message to publish")
-    parser.add_argument("-us", "--unsubscribe", type=str, help="Unsubscribe from a Channel")
-    parser.add_argument("-here", "--here-now", type=str, help="Unsubscribe from a Channel")
-    parser.add_argument("-dm", "--device-manager", type=str, help="Attach a local Device Manager at a file")
+    """Create argument parser
+
+    Returns:
+        parser: parser without arguments
+    """
     return parser
--- tests/test_v1.py
+++ tests/test_v1.py
@@ -1,18 +1,51 @@
 """Test file for cli/v1.py."""
+from logging import DEBUG
+from unittest import TestCase
+
 from pubnub_python_tools.cli.v1 import get_parser, create_parser
 from pubnub_python_tools.logger.logging_config import set_logger
-from unittest import TestCase
-import sys
 
-# Create a logger if needed for testing cases
-from logging import DEBUG
-LOG = set_logger("test_v1", DEBUG)  # Defaults as INFO
+LOG = set_logger("test_v1", DEBUG)  # Create a logger if needed. Default: INFO
 
-class RunMainAppTests(TestCase):
+class TestCliV1(TestCase):
+    """Test cli/v1.py
+    """
+    def test_created_parser(self):
+        """Test created parser.
+        """
+        parser = create_parser()
+        self.assertIsNotNone(parser)
 
-    def setUp(self):
-        self.parser = create_parser()
+    def test_parsed_arguments_short_flags(self):
+        """Test parsed arguments.
+        """
+        parser = create_parser()
+        parsed = parser.parse_args(['-sk', 'sk.test'])
+        self.assertEqual(parsed.subscribe_key, 'sk.test')
+        parsed = parser.parse_args(['-pk', 'pk.test'])
+        self.assertEqual(parsed.publish_key, 'pk.test')
+        parsed = parser.parse_args(['-u', 'u.test'])
+        self.assertEqual(parsed.uuid, 'u.test')
+        parsed = parser.parse_args(['-pres'])
+        self.assertEqual(parsed.presence, True)
+        parsed = parser.parse_args(['-p', 'p.test'])
+        self.assertEqual(parsed.publish, 'p.test')
+        parsed = parser.parse_args(['-m', 'm.test'])
+        self.assertEqual(parsed.message, 'm.test')
+        parsed = parser.parse_args(['-us', 'us.test'])
+        self.assertEqual(parsed.unsubscribe, 'us.test')
+        parsed = parser.parse_args(['-here', 'here.test'])
+        self.assertEqual(parsed.here_now, 'here.test')
+        parsed = parser.parse_args(['-dm', 'dm.test'])
+        self.assertEqual(parsed.dev_man, 'dm.test')
 
-    def test_something(self):
-        parsed = self.parser.parse_args(['--subscribe-key', 'test'])
+    # you can also test long flags but it's redundant.
+
+    def test_get_parser(self):
+        """Test get_parser() with arguments
+        """
+        parsed = get_parser(['--subscribe-key', 'test'])
         self.assertEqual(parsed.subscribe_key, 'test')
+        # You can also test all arguments but it's redundant
+        # As long as one works, all should work
+        # Unless you want to test for types or count arguments, etc.
