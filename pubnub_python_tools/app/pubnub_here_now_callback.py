6579e77ec967b4421d9c650f27c5a9eae2fae931
Sergio Munoz <sergio.munoz@pubnub.com>
2022-09-08 19:57:34 -0400

    Removed pubnub-python-tools (this package) from requirements.txt

--- pubnub_python_tools/app/pubnub_here_now_callback.py
+++ pubnub_python_tools/app/pubnub_here_now_callback.py
@@ -1,21 +1,24 @@
-"""Pubnub HereNow Callback"""
+"""Pubnub HereNow Callback."""
 from ..logger.logging_config import get_logger
 
 LOG = get_logger()
 
 def here_now_callback(result, status):
-    LOG.debug("Calling here_now")
+    """here_now function callback resolver.
+
+    Args:
+        result (PNResult): here_now result envelope.
+        status (PNStatus): here_now status envelope.
+    """
     if status.is_error():
         # handle error
-        LOG.error(status)
-        return
+        LOG.error("here_now_callback error status: %s", status)
 
-    LOG.debug("here_now response: %s", result)
     for channel_data in result.channels:
-        print("---")
-        print("channel: %s" % channel_data.channel_name)
-        print("occupancy: %s" % channel_data.occupancy)
-
-        print("occupants: %s" % channel_data.channel_name)
+        print(f'channel: {channel_data.channel_name}')
+        print(f'occupancy: {channel_data.occupancy}')
+        print(f'occupants: {channel_data.channel_name}')
     for occupant in channel_data.occupants:
-        print("uuid: %s, state: %s" % (occupant.uuid, occupant.state))
+        print(f'uuid: {occupant.uuid}, state: {occupant.state}')
+
+    LOG.info("here_now_callback response: %s", result)
