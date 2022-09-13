91404d3bd5a6dbd6e571d3d4a0d48c789662c09a
Sergio Munoz <sergio.munoz@pubnub.com>
2022-09-13 18:14:19 -0400

    Updated tests import folder.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ import os
 import sys
 
 ROOT_DIR = str(os.path.dirname(os.path.abspath(__file__)).rsplit("/tests")[0])
-sys.path.append(f"{ROOT_DIR}/pubnub_python_tools")
+sys.path.append(f"{ROOT_DIR}/src/pubnub_python_tools")
