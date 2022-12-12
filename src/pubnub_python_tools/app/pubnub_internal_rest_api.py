a7bd6d73591098984dc31c8caedd1bf6971af664
Sergio M <110481361+sergio-munoz@users.noreply.github.com>
2022-12-12 15:36:55 -0500

    Added pubnub_internal_rest_api selected methods and tests to get usage per app.

---
+++ src/pubnub_python_tools/app/pubnub_internal_rest_api.py
@@ -0,0 +1,78 @@
+"""Interact with PubNub's internal api."""
+import requests
+import json
+
+
+def get(url, params, json):
+    """Get wrapper."""
+    return requests.get(url=url, params=params, json=json)
+
+
+def authenticate(email, password):
+    """Authenticate using PubNub internal rest api."""
+    URL = 'https://admin.pubnub.com/api/me'
+    HEADER = {'Content-Type': 'application/json'}
+    DATA_RAW = '{"email": "' + email + '", "password": "' + password + '"}'
+
+    response = requests.post(url=URL, headers=HEADER, data=DATA_RAW)
+    decoded = str(response.content.decode())
+    parsed = json.loads(decoded)
+    token = parsed['result']['token']
+    user = parsed['result']['user']['id']
+
+    return (user, token)
+
+
+def get_accounts(user, token):
+    """Authenticate using PubNub internal rest api."""
+    URL = f'https://admin.pubnub.com/api/accounts?user_id={user}'
+    HEADER = {'X-Session-Token': token}
+
+    response = requests.get(url=URL, headers=HEADER)
+    decoded = str(response.content.decode())
+    parsed = json.loads(decoded)
+    if 'code' in parsed:
+        raise Exception("Invalid credentials")
+    accounts = parsed['result']['accounts']
+    return accounts
+
+
+def get_accounts_ids(accounts) -> list:
+    ids = []
+    for account in accounts:
+        ids.append(account['id'])
+    return ids
+
+
+def get_apps(account_id, token):
+    """Get apps using PubNub internal rest api."""
+    URL = f'https://admin.pubnub.com/api/apps?owner_id={account_id}&no_keys=1'
+    HEADER = {'X-Session-Token': token}
+
+    response = requests.get(url=URL, headers=HEADER)
+    decoded = str(response.content.decode())
+    parsed = json.loads(decoded)
+    if 'code' in parsed:
+        raise Exception("Invalid credentials")
+    apps = parsed['result']
+    return apps
+
+
+def get_apps_ids(apps) -> list:
+    ids = []
+    for app in apps:
+        ids.append(app['id'])
+    return ids
+
+
+def get_app_based_usage(app_id, token, usage_type, start_date, end_date):
+    """Get app based usage using PubNub internal rest api."""
+    URL = f'https://admin.pubnub.com/api/v4/services/usage/legacy/usage?app_id={app_id}&usageType={usage_type}&file_format=json&start={start_date}&end={end_date}'
+    HEADER = {'X-Session-Token': token}
+
+    response = requests.get(url=URL, headers=HEADER)
+    decoded = str(response.content.decode())
+    parsed = json.loads(decoded)
+    if 'code' in parsed:
+        raise Exception("Invalid credentials")
+    return parsed
---
+++ tests/test_internal_rest_api.py
@@ -0,0 +1,101 @@
+"""Test file for test_internal_rest_api.py."""
+from unittest import TestCase
+from logging import DEBUG
+
+from src.pubnub_python_tools.logger.logging_config import set_logger
+from src.pubnub_python_tools.app.pubnub_internal_rest_api import authenticate, get_accounts, get_accounts_ids, get_apps, get_apps_ids, get_app_based_usage
+
+LOG = set_logger("test_request_function", DEBUG)  # Create a logger if needed. 
+
+
+class TestPubNubInternalRestAPI(TestCase):
+
+    def test_authenticate(self):
+        """Tests authenticate function."""
+        LOG.info("Testing authenticagte()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+
+        resp = authenticate(EMAIL, PASSWORD)
+        LOG.debug(resp)
+
+        # Expect the id to be 594221
+        self.assertEqual(resp[0], 594221)
+        # Expect token not to be empty
+        self.assertIsNotNone(resp[1])
+
+    def test_get_accounts(self):
+        """Tests get_accounts function."""
+        LOG.info("Testing authenticagte()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+
+        resp = get_accounts(auth[0], auth[1])
+        LOG.debug(resp)
+        self.assertIsNotNone(resp)
+
+    def test_get_accounts_ids(self):
+        """Tests get_accounts_ids function."""
+        LOG.info("Testing get_accounts_ids()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+        accounts = get_accounts(auth[0], auth[1])
+
+        resp = get_accounts_ids(accounts)
+        LOG.debug(resp)
+        self.assertEquals(resp, [594172])
+
+    def test_get_accounts_invalid(self):
+        """Tests get_accounts invalid function."""
+        LOG.info("Testing authenticagte()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+
+        # Add 1 to the user_id
+        with self.assertRaises(Exception):  # TODO - Add specific exception
+            get_accounts(auth[0]+1, auth[1])
+
+    def test_get_apps(self):
+        """Tests get_apps function."""
+        LOG.info("Testing get_apps()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+        accounts = get_accounts(auth[0], auth[1])
+        accounts_ids = get_accounts_ids(accounts)
+
+        resp = get_apps(accounts_ids[0], auth[1])
+        LOG.debug(resp)
+        self.assertIsNotNone(resp)
+
+    def test_get_apps_ids(self):
+        """Tests get_apps_ids function."""
+        LOG.info("Testing get_apps_ids()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+        accounts = get_accounts(auth[0], auth[1])
+        accounts_ids = get_accounts_ids(accounts)
+        apps = get_apps(accounts_ids[0], auth[1])
+
+        resp = get_apps_ids(apps)
+        LOG.debug(resp)
+        self.assertEquals(resp, [35415338])
+
+    def test_get_app_based_usage(self):
+        """Tests get_app_based_usage function."""
+        LOG.info("Testing get_app_based_usage()")
+        EMAIL = "6unrdlsk2@mozmail.com"
+        PASSWORD = "=T[(_VD9<?Fv=k>d"
+        auth = authenticate(EMAIL, PASSWORD)
+        accounts = get_accounts(auth[0], auth[1])
+        accounts_ids = get_accounts_ids(accounts)
+        apps = get_apps(accounts_ids[0], auth[1])
+        apps_ids = get_apps_ids(apps)
+
+        resp = get_app_based_usage(apps_ids[0], auth[1], "transaction", "2022-12-01", "2021-12-02")
+        LOG.debug(resp)
+        self.assertIsNotNone(resp)
